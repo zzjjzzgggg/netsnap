0f5bd2e61a3ec23f592f35f5a31c868c4f551921
jzzhao_on_pad <jzzhao@sei.xjtu.edu.cn>
2013-02-20 23:02:04 +0800

    change random

--- glib/random.h
+++ glib/random.h
@@ -12,61 +12,50 @@ public:
 		while(sum<th) sum+=list[++id]+esp;
 		return id;
 	}
-	static int sample(const TIntFltH &map){
-		AssertR(!map.Empty(), TStr::Fmt("I cannot sample from an empty set."));
-		double sum=0, esp=1E-6;
-		TIntFltH::TIter it;
-		for(it=map.BegI(); it<map.EndI(); it++) sum+=it.GetDat()+esp;
-		double th=TInt::Rnd.GetUniDev()*sum;
-		it=map.BegI();
-		sum=it.GetDat()+esp;
-		while(sum<th) {
-			it++;
-			sum+=it.GetDat()+esp;
-		}
-		return it.GetKey();
-	}
-	static int sample(const TIntH &map){
-			AssertR(!map.Empty(), TStr::Fmt("I cannot sample from an empty set."));
-			double sum=0, esp=1E-6;
-			TIntH::TIter it;
-			for(it=map.BegI(); it<map.EndI(); it++) sum+=it.GetDat()+esp;
-			double th=TInt::Rnd.GetUniDev()*sum;
-			it=map.BegI();
-			sum=it.GetDat()+esp;
-			while(sum<th) {
-				it++;
-				sum+=it.GetDat()+esp;
-			}
-			return it.GetKey();
-		}
-
 };
 
 namespace TRandom{
-template<class TKey> TKey& Sample(const TVec<TKey> samples, TKey& rst);
-template<class TKey> TKey& Sample(const THashSet<TKey> samples, TKey& rst);
-template<class TKey> void Choose(const TVec<TKey> samples, TVec<TKey>& rst, const int n);
-template<class TKey> void Choose(const THashSet<TKey> samples, THashSet<TKey>& rst, const int n);
+template<class TKey> TKey& Sample(const TVec<TKey>& samples, TKey& rst);
+template<class TKey> TKey& Sample(const THashSet<TKey>& samples, TKey& rst);
+template<class TKey> TKey& Sample(const THashSet<TKey, TFlt>& samples, TKey& rst);
+template<class TKey> void Choose(const TVec<TKey>& samples, TVec<TKey>& rst, const int n);
+template<class TKey> void Choose(const THashSet<TKey>& samples, THashSet<TKey>& rst, const int n);
 
 
 
 template<class TKey>
-TKey& Sample(const TVec<TKey> samples, TKey& rst){
+TKey& Sample(const TVec<TKey>& samples, TKey& rst){
 	AssertR(samples.Len()!=0, TStr::Fmt("Empty samples cannot be sampled!"));
 	rst=samples[TInt::Rnd.GetUniDevInt(samples.Len())];
 	return rst;
 }
 
 template<class TKey>
-TKey& Sample(const THashSet<TKey> samples, TKey& rst){
+TKey& Sample(const THashSet<TKey>& samples, TKey& rst){
 	AssertR(samples.Len()!=0, TStr::Fmt("Empty samples cannot be sampled!"));
 	rst=samples[TInt::Rnd.GetUniDevInt(samples.Len())];
 	return rst;
 }
 
 template<class TKey>
-void Choose(const TVec<TKey> samples, TVec<TKey>& rst, const int n){
+TKey& Sample(const THash<TKey, TFlt>& samples, TKey& rst){
+	AssertR(!samples.Empty(), TStr::Fmt("I cannot sample from an empty set."));
+	double sum=0, esp=1E-6;
+	TIntFltH::TIter it;
+	for(it=samples.BegI(); it<samples.EndI(); it++) sum+=it.GetDat()+esp;
+	double th=TInt::Rnd.GetUniDev()*sum;
+	it=samples.BegI();
+	sum=it.GetDat()+esp;
+	while(sum<th) {
+		it++;
+		sum+=it.GetDat()+esp;
+	}
+	rst = it.GetKey();
+	return rst;
+}
+
+template<class TKey>
+void Choose(const TVec<TKey>& samples, TVec<TKey>& rst, const int n){
 	int L=samples.Len();
 	AssertR(n<=L, TStr::Fmt("%d > sample length %d!", n, L));
 	rst.Clr();
@@ -79,7 +68,7 @@ void Choose(const TVec<TKey> samples, TVec<TKey>& rst, const int n){
 }
 
 template<class TKey>
-void Choose(const THashSet<TKey> samples, THashSet<TKey>& rst, const int n){
+void Choose(const THashSet<TKey>& samples, THashSet<TKey>& rst, const int n){
 	int L=samples.Len();
 	AssertR(n<=L, TStr::Fmt("%d > sample length %d!", n, L));
 	rst.Clr();
